252cea945d89f83699e4c5b3d796a03da4f48c8f
rgalishnikov <rgalishnikov@skbkontur.ru>
2016-12-16 14:58:30 +0500

    Dedup instead of an overload

--- lib/pagination.ex
+++ lib/pagination.ex
@@ -1,13 +1,13 @@
 defmodule Pagination do   	
   defp f(i, maxElement) when i > maxElement + 1, do: {["...", i], i}  
-  defp f(i, maxElement) when i > maxElement, do: {[i], i}  
-  defp f(_, maxElement), do: {[], maxElement}
+  defp f(i, maxElement) when i > maxElement, do: {[i], i}    
   def get(total, selected) do
-  	pages = [1..3, selected-1..selected+1, total-2..total] 
-		  	|> Enum.flat_map(&(&1))
-		  	|> Enum.filter(&(&1 > 0 and &1 <= total)) 
-		  	|> Enum.sort			  	
-  	{result, _} = Enum.flat_map_reduce(pages, 0, &f/2)
-  	result
+    pages = [1..3, selected-1..selected+1, total-2..total] 
+        |> Enum.flat_map(&(&1))
+        |> Enum.filter(&(&1 > 0 and &1 <= total)) 
+        |> Enum.sort
+        |> Enum.dedup
+    {result, _} = Enum.flat_map_reduce(pages, 0, &f/2)
+    result
   end
 end
