705e7ad5658dbe55629a0dc8f315b96c7a106142
rgalishnikov <rgalishnikov@skbkontur.ru>
2016-12-16 14:34:02 +0500

    Rollback to the initial approach

--- lib/pagination.ex
+++ lib/pagination.ex
@@ -1,9 +1,10 @@
-defmodule Pagination do
+defmodule Pagination do   	
   defp f(i, maxElement) when i > maxElement + 1, do: {["...", i], i}  
   defp f(i, maxElement) when i > maxElement, do: {[i], i}  
   defp f(_, maxElement), do: {[], maxElement}
   def get(total, selected) do
-  	pages = [1, 2, 3, selected - 1, selected, selected + 1, total - 2, total - 1, total] 		  	
+  	pages = [1..3, selected-1..selected+1, total-2..total] 
+		  	|> Enum.flat_map(&(&1))
 		  	|> Enum.filter(&(&1 > 0 and &1 <= total)) 
 		  	|> Enum.sort			  	
   	{result, _} = Enum.flat_map_reduce(pages, 0, &f/2)
